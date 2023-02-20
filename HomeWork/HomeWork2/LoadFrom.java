5f831a17fb2594d18179537cc7d8171d2bb3628e
ivano <ivanov399@mail.ru>
2023-02-20 19:31:59 +0300

    Initial commit

---
+++ HomeWork/HomeWork2/LoadFrom.java
@@ -0,0 +1,8 @@
+package HomeWork2;
+
+import java.io.Serializable;
+
+public interface LoadFrom
+{
+    Serializable load(String path);
+}
---
+++ HomeWork/HomeWork2/SaveAs.java
@@ -0,0 +1,8 @@
+package HomeWork2;
+
+import java.io.Serializable;
+
+public interface SaveAs
+{
+    void save(String path, Serializable obj);
+}
